ac6cb365516271d995e1a490cb6659bab1bcdfb1
Andrew Glover <ajglover@gmail.com>
2013-08-29 13:57:42 -0400

    init commit

---
+++ src/com/b50/sqs/SQSQueueAdapter.java
@@ -0,0 +1,84 @@
+package com.b50.sqs;
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.model.*;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: aglover
+ * Date: 8/27/13
+ * Time: 1:56 PM
+ * <p/>
+ * Sample code: https://github.com/aws/aws-sdk-java/blob/master/src/samples/AmazonSimpleQueueService/SimpleQueueServiceSample.java
+ * JavaDocs: http://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/sqs/AmazonSQS.html
+ */
+public class SQSQueueAdapter {
+    private final ExecutorService pool = Executors.newFixedThreadPool(10);
+    private AmazonSQS sqs;
+    private String queueURL;
+
+    public SQSQueueAdapter(final AmazonSQS sqs, final String queueURL) {
+        this.sqs = sqs;
+        this.queueURL = queueURL;
+    }
+
+    /**
+     * Assumes east coast region!
+     *
+     * @param awsKey
+     * @param awsSecret
+     * @param queueName
+     */
+    public SQSQueueAdapter(final String awsKey, final String awsSecret, final String queueName) {
+        sqs = new AmazonSQSClient(new BasicAWSCredentials(awsKey, awsSecret));
+        queueURL = sqs.createQueue(new CreateQueueRequest(queueName)).getQueueUrl();
+    }
+
+    public void receive(final MessageReceivedCallback callback) {
+        receive(sqs, queueURL, callback);
+    }
+
+    public void send(final String message) {
+        send(sqs, queueURL, message, null);
+    }
+
+    public void send(final String message, final MessageSentCallback callback) {
+        send(sqs, queueURL, message, callback);
+    }
+
+    private void receive(final AmazonSQS sqs, final String queueURL, final MessageReceivedCallback callback) {
+        pool.execute(new Runnable() {
+            @Override
+            public void run() {
+                final List<Message> messages = sqs.receiveMessage(
+                        new ReceiveMessageRequest(queueURL).withMaxNumberOfMessages(10).withWaitTimeSeconds(20)).getMessages();
+                if (messages.size() > 0) {
+                    for (final Message message : messages) {
+                        callback.onReceive(message.getMessageId(), message.getBody());
+                        sqs.deleteMessage(new DeleteMessageRequest(queueURL, message.getReceiptHandle()));
+                    }
+                }
+            }
+        });
+    }
+
+    private void send(final AmazonSQS sqs, final String queueURL, final String message, final MessageSentCallback callback) {
+        pool.execute(new Runnable() {
+            @Override
+            public void run() {
+                SendMessageResult res = sqs.sendMessage(new SendMessageRequest(queueURL, message));
+                if (callback != null) {
+                    callback.onSend(res.getMessageId());
+                }
+            }
+        });
+    }
+
+
+}
---
+++ test/test/com/b50/sqs/SQSAdapterTest.java
@@ -0,0 +1,86 @@
+package test.com.b50.sqs;
+
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.model.*;
+import com.b50.sqs.MessageReceivedCallback;
+import com.b50.sqs.MessageSentCallback;
+import com.b50.sqs.SQSQueueAdapter;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: aglover
+ * Date: 8/27/13
+ * Time: 1:54 PM
+ */
+public class SQSAdapterTest {
+
+    @Test
+    public void testAdapterReceive() throws Exception {
+
+        AmazonSQSClient mockClient = mock(AmazonSQSClient.class);
+        CreateQueueResult mockQueueResult = mock(CreateQueueResult.class);
+        when(mockClient.createQueue(any(CreateQueueRequest.class))).thenReturn(mockQueueResult);
+        when(mockQueueResult.getQueueUrl()).thenReturn("URL");
+        ReceiveMessageResult receiveMessageResult = mock(ReceiveMessageResult.class);
+        when(mockClient.receiveMessage(any(ReceiveMessageRequest.class))).thenReturn(receiveMessageResult);
+
+        Message simpleMessage = new Message();
+        simpleMessage.setBody("TESTING 1,2,3");
+        simpleMessage.setMessageId("1");
+        List<Message> mockedMsgs = new LinkedList<Message>();
+        mockedMsgs.add(simpleMessage);
+        when(receiveMessageResult.getMessages()).thenReturn(mockedMsgs);
+
+        SQSQueueAdapter sqs = new SQSQueueAdapter(mockClient, "some queue");
+        assertNotNull("sqs object was null", sqs);
+
+        final boolean[] wasReceived = {false};
+        sqs.receive(new MessageReceivedCallback() {
+            @Override
+            public void onReceive(String id, String message) {
+                wasReceived[0] = true;
+                assertEquals("id should be 1", "1", id);
+                assertEquals("message should be TESTING 1,2,3", "TESTING 1,2,3", message);
+            }
+        }
+        );
+
+        Thread.sleep(2000);
+        assertEquals("wasReceived was not true", true, wasReceived[0]);
+
+    }
+
+    @Test
+    public void testAdapterSend() throws Exception {
+        AmazonSQSClient mockClient = mock(AmazonSQSClient.class);
+        SendMessageResult mockResult = mock(SendMessageResult.class);
+        when(mockClient.sendMessage(any(SendMessageRequest.class))).thenReturn(mockResult);
+        when(mockResult.getMessageId()).thenReturn("TEST");
+
+        SQSQueueAdapter sqs = new SQSQueueAdapter(mockClient, "some queue");
+        assertNotNull("sqs object was null", sqs);
+
+        final boolean[] wasReceived = {false};
+        sqs.send("Message", new MessageSentCallback() {
+            @Override
+            public void onSend(String messageId) {
+                wasReceived[0] = true;
+                assertEquals("should be TEST", "TEST", messageId);
+            }
+        });
+
+        Thread.sleep(2000);
+        assertEquals("wasReceived was not true", true, wasReceived[0]);
+    }
+}
